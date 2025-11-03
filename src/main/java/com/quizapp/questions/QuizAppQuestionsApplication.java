aad255b4da83becba9964b3678794922a6307eec
Savina Dzhenimova <129115347+SavinaDzhenimova@users.noreply.github.com>
2025-11-03 12:00:35 +0200

    Create public repository for REST API with questions for quiz app.

---
+++ src/main/java/com/quizapp/questions/QuizAppQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.quizapp.questions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAppQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizAppQuestionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quizapp/questions/QuizAppQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizapp.questions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
