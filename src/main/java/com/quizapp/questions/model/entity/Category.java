c57f271ef56f3c6bdc74e32873c5ded12d4d3164
Savina Dzhenimova <129115347+SavinaDzhenimova@users.noreply.github.com>
2025-11-03 13:02:40 +0200

    Add categories and questions tables and entity classes

---
+++ src/main/java/com/quizapp/questions/model/entity/Category.java
@@ -0,0 +1,34 @@
+package com.quizapp.questions.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Question> questions = new ArrayList<>();
+}
---
+++ src/main/java/com/quizapp/questions/model/entity/Question.java
@@ -0,0 +1,35 @@
+package com.quizapp.questions.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "questions")
+@Data // включва @Getter, @Setter, @ToString, @EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "question_text", nullable = false)
+    private String questionText;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @Column(name = "correct_answer", nullable = false)
+    private String correctAnswer;
+
+    @ElementCollection
+    private List<String> options;
+}
