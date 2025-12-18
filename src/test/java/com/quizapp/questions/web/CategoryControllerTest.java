c8cbe4aaa0653d2986a784bd3820667996c74c60
Savina Dzhenimova <129115347+SavinaDzhenimova@users.noreply.github.com>
2025-12-18 16:16:32 +0200

    Created tests for get by id, post and put methods in category conroller class

--- src/test/java/com/quizapp/questions/web/CategoryControllerTest.java
+++ src/test/java/com/quizapp/questions/web/CategoryControllerTest.java
@@ -1,24 +1,26 @@
 package com.quizapp.questions.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.quizapp.questions.model.dto.category.AddCategoryDTO;
 import com.quizapp.questions.model.dto.category.CategoryDTO;
 import com.quizapp.questions.model.dto.category.CategoryPageDTO;
+import com.quizapp.questions.model.dto.category.UpdateCategoryDTO;
 import com.quizapp.questions.service.interfaces.CategoryService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Collections;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -28,17 +30,26 @@ public class CategoryControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockitoBean
     private CategoryService categoryService;
 
-    @Test
-    void getAllCategories_ShouldReturnPage_WhenCategoriesFound() throws Exception {
-        CategoryDTO categoryDTO = CategoryDTO.builder()
+    private CategoryDTO categoryDTO;
+
+    @BeforeEach
+    void setUp() {
+        this.categoryDTO = CategoryDTO.builder()
                 .id(1L)
                 .name("Maths")
                 .description("Description")
                 .build();
-        CategoryPageDTO page = new CategoryPageDTO(List.of(categoryDTO));
+    }
+
+    @Test
+    void getAllCategories_ShouldReturnPage_WhenCategoriesFound() throws Exception {
+        CategoryPageDTO page = new CategoryPageDTO(List.of(this.categoryDTO));
 
         Pageable pageable = PageRequest.of(0, 10, Sort.by("id"));
 
@@ -70,5 +81,48 @@ public class CategoryControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.categories").isArray())
                 .andExpect(jsonPath("$.categories").isEmpty());
+    }
+
+    @Test
+    void getCategoryById_ShouldReturnCategoryDTO_WhenCategoryFound() throws Exception {
+        when(this.categoryService.getCategoryById(1L))
+                .thenReturn(this.categoryDTO);
+
+        this.mockMvc.perform(get("/api/categories/{id}", 1L))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("Maths"))
+                .andExpect(jsonPath("$.description").value("Description"));
+    }
+
+    @Test
+    void addCategory_ShouldCreateNewCategory_WhenInputDataIsValid() throws Exception {
+        AddCategoryDTO addCategoryDTO = AddCategoryDTO.builder()
+                .name("Music")
+                .description("Description")
+                .build();
+
+        this.mockMvc.perform(post("/api/categories")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.objectMapper.writeValueAsString(addCategoryDTO)))
+                .andExpect(status().isCreated());
+
+        verify(this.categoryService, times(1)).addCategory(addCategoryDTO);
+    }
+
+    @Test
+    void updateCategory_ShouldUpdateCategory_WhenCategoryExists() throws Exception {
+        UpdateCategoryDTO updateCategoryDTO = UpdateCategoryDTO.builder()
+                .id(1L)
+                .name("Maths")
+                .description("New description")
+                .build();
+
+        this.mockMvc.perform(put("/api/categories/{id}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.objectMapper.writeValueAsString(updateCategoryDTO)))
+                .andExpect(status().isOk());
+
+        verify(this.categoryService, times(1)).updateCategory(1L, updateCategoryDTO);
     }
 }
