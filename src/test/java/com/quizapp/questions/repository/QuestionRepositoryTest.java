5f6b2d46a2b27694f455e2c849ec450a31bbb436
Savina Dzhenimova <129115347+SavinaDzhenimova@users.noreply.github.com>
2025-12-18 11:31:17 +0200

    Created tests for question repository to test methods find all with specifications and pageable and find by category id

---
+++ src/test/java/com/quizapp/questions/repository/QuestionRepositoryTest.java
@@ -0,0 +1,95 @@
+package com.quizapp.questions.repository;
+
+import com.quizapp.questions.model.entity.Category;
+import com.quizapp.questions.model.entity.Question;
+import com.quizapp.questions.repository.spec.QuestionSpecifications;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class QuestionRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @BeforeEach
+    void setUp() {
+        Category category = Category.builder()
+                .name("Maths")
+                .description("Description")
+                .build();
+        this.entityManager.persist(category);
+
+        Question question = Question.builder()
+                .category(category)
+                .questionText("Question")
+                .correctAnswer("A")
+                .options(List.of("A", "B", "C", "D"))
+                .build();
+        this.questionRepository.save(question);
+
+        this.entityManager.flush();
+    }
+
+    @Test
+    void findAll_ShouldReturnQuestionsWithSpecificationAndPageable_WhenQuestionsFound() {
+        Specification<Question> spec = Specification
+                .allOf(QuestionSpecifications.hasCategory(1L))
+                .and(QuestionSpecifications.hasText("Question"));
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Page<Question> page = this.questionRepository.findAll(spec, pageable);
+
+        assertThat(page).isNotEmpty();
+        assertThat(page.getContent().get(0).getQuestionText()).isEqualTo("Question");
+        assertThat(page.getContent().get(0).getCorrectAnswer()).isEqualTo("A");
+        assertThat(page.getContent().get(0).getOptions()).containsExactly("A", "B", "C", "D");
+        assertThat(page.getContent().get(0).getCategory().getName()).isEqualTo("Maths");
+        assertThat(page.getContent().get(0).getCategory().getDescription()).isEqualTo("Description");
+    }
+
+    @Test
+    void findAll_ShouldReturnEmpty_WhenQuestionsNotFound() {
+        Specification<Question> spec = Specification
+                .allOf(QuestionSpecifications.hasCategory(1L))
+                .and(QuestionSpecifications.hasText("Missing"));
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Page<Question> page = this.questionRepository.findAll(spec, pageable);
+
+        assertThat(page).isEmpty();
+    }
+
+    @Test
+    void findByCategoryId_ShouldReturnQuestions_WhenCategoryFound() {
+        List<Question> questions = this.questionRepository.findByCategoryId(1L);
+
+        assertThat(questions).isNotEmpty();
+        assertThat(questions.get(0).getQuestionText()).isEqualTo("Question");
+        assertThat(questions.get(0).getCorrectAnswer()).isEqualTo("A");
+        assertThat(questions.get(0).getOptions()).containsExactly("A", "B", "C", "D");
+        assertThat(questions.get(0).getCategory().getName()).isEqualTo("Maths");
+        assertThat(questions.get(0).getCategory().getDescription()).isEqualTo("Description");
+    }
+
+    @Test
+    void findByCategoryId_ShouldReturnEmptyQuestions_WhenCategoryNotFound() {
+        List<Question> questions = this.questionRepository.findByCategoryId(5L);
+
+        assertThat(questions).isEmpty();
+    }
+}
