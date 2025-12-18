487db694b4383b65257c2f00671116946ac407e2
Savina Dzhenimova <129115347+SavinaDzhenimova@users.noreply.github.com>
2025-12-18 11:12:49 +0200

    Created tests for category repository class

---
+++ src/test/java/com/quizapp/questions/repository/CategoryRepositoryTest.java
@@ -0,0 +1,74 @@
+package com.quizapp.questions.repository;
+
+import com.quizapp.questions.model.entity.Category;
+import com.quizapp.questions.repository.spec.CategorySpecifications;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class CategoryRepositoryTest {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @BeforeEach
+    void setUp() {
+        Category category = Category.builder()
+                .name("Maths")
+                .description("Description")
+                .questions(new ArrayList<>())
+                .build();
+
+        this.categoryRepository.save(category);
+    }
+
+    @Test
+    void findAll_ShouldReturnEmptyPage_WhenCategoriesNotFound() {
+        Specification<Category> spec = Specification
+                .allOf(CategorySpecifications.hasName("Music"));
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Page<Category> page = this.categoryRepository.findAll(spec, pageable);
+
+        assertThat(page).isEmpty();
+    }
+
+    @Test
+    void findAll_ShouldReturnCategoriesWithSpecificationAndPageable_WhenCategoriesFound() {
+        Specification<Category> spec = Specification
+                .allOf(CategorySpecifications.hasName("Maths"));
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Page<Category> page = this.categoryRepository.findAll(spec, pageable);
+
+        assertThat(page).isNotEmpty();
+        assertThat(page.getContent().get(0).getName()).isEqualTo("Maths");
+        assertThat(page.getContent().get(0).getDescription()).isEqualTo("Description");
+    }
+
+    @Test
+    void findByName_ShouldReturnCategory_WhenCategoryFound() {
+        Optional<Category> optionalCategory = this.categoryRepository.findByName("Maths");
+
+        assertThat(optionalCategory).isNotEmpty();
+        assertThat(optionalCategory.get().getName()).isEqualTo("Maths");
+    }
+
+    @Test
+    void findByName_ShouldReturnEmpty_WhenCategoryNotFound() {
+        Optional<Category> optionalCategory = this.categoryRepository.findByName("Music");
+
+        assertThat(optionalCategory).isEmpty();
+    }
+}
